350b05f0756a0894075a7f459661c5ef4d9d15ee
Odell <odell.purifoy@gmail.com>
2018-12-13 14:05:50 -0500

    Part 2 for comments buildout

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,14 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment]).permit(:name, :body)
+    @comment = @post.comments.create!(params.require(:comment).permit(:name, :body))
     redirect_to post_path(@post)
   end
 
-  def update; end
-
-  def destroy; end
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
 end
